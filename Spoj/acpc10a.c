9f1b7f2a2baf0819a75cee7d70e86502cf5569e9
Asmita <asmita0941@gmail.com>
2013-06-08 20:07:50 +0530

    8June13

--- Spoj/acpc10a.c
+++ Spoj/acpc10a.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
- 
+
 int main (void)
 {
-        int a, b, c;
 	while (1)
 	{
-                scanf ("%d%d%d", &a, &b, &c);
-		if (!(a || b || c)) 
-		  break;
-                if (c - b == b - a) 
-		  printf ("AP %d\n", (c<<1) - b);
-		else 
-		  printf ("GP %d\n", (c * c) / b);
-        }
-        return 0;
+		int a, b, c;
+		scanf ("%d%d%d", &a, &b, &c);
+		if (a == 0 && b == 0 && c == 0)
+			break;
+		if (c - b == b - a)
+			printf ("AP %d\n", (c << 1) - b);
+		else
+			printf ("GP %d\n", (c * c) / b);
+	}
+	return 0;
 }
